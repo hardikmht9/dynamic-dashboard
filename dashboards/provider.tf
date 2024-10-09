16f134eaae54872008737df9d054b8c6bd3c42ec
hardikmht9 <hardikmehta99999@gmail.com>
2024-10-09 17:57:34 +0530

    done4

--- dashboards/provider.tf
+++ dashboards/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the New Relic provider
 provider "newrelic" {
-#  account_id = "4438271"
-#   api_key = "NRAK-XXL9XIK9SB065A8EU75N1D14JXV"    
+ account_id = "4438271"
+  api_key = "NRAK-XXL9XIK9SB065A8EU75N1D14JXV"    
   region = "US"                     
 }
